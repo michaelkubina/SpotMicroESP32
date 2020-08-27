7bafc1a0c9570819c1feda5fc3ff3385e78cd560
Maarten Weyn <maarten@wesdec.be>
2020-08-27 21:43:25 +0200

    servomotor test application

--- code/servomotor_test/servomotor_test.ino
+++ code/servomotor_test/servomotor_test.ino
@@ -8,14 +8,16 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-
-#define MIN_PULSE   140
-#define ANGLE_FACTOR  1
+#include <CommandParser.h>
 
 #define PWM_SERVO_ADDR    0x40
 Adafruit_PWMServoDriver PWM = Adafruit_PWMServoDriver(PWM_SERVO_ADDR);
 
-int pos = MIN_PULSE;
+
+typedef CommandParser<> MyCommandParser;
+MyCommandParser servoCommandParser;
+int pulse = 170;
+int servo_id = 0;
 
 
 
@@ -30,7 +32,16 @@ void setup() {
   PWM.setPWMFreq(50);
 
   Serial.begin(115200);
-  Serial.println("< ESP32 FireBeetle ESP32 Ready >");
+
+  while (!Serial);
+
+  servoCommandParser.registerCommand("s", "uu", &cmd_servo);
+  Serial.println("registered command: s <uint64> <uint64> ");
+  Serial.println("example: s 0 150");
+  servoCommandParser.registerCommand("sleep", "", &servosSleep);
+  Serial.println("registered command: sleep ");
+  servoCommandParser.registerCommand("wake", "", &servosWake);
+  Serial.println("registered command: wake ");
   Serial.println("");
 }
 
@@ -41,51 +52,70 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(100);                       // wait for a second
 
-  checkSerialInput();
-  writePWM();
+  if (checkSerialInput())
+    writePWM();
 }
 
-void checkSerialInput(){
-    if (Serial.available() > 0) {
-      char symbol = Serial.read();
-      Serial.print(symbol);
-    switch (symbol) {
-      case '0':
-        pos = 0;
-        break;
-      case '3':
-        pos = 30;
-        break;
-      case '9':
-        pos = 90;
-        break;
-      case '5':
-        pos = 150;
-        break;
-      case '8':
-        pos = 180;
-        break;
-      case '+':
-        pos += 5;
-        break;
-      case '-':
-        pos -= 5;
-        break;
-    }
+void cmd_servo(MyCommandParser::Argument *args, char *response) {
+  servo_id = args[0].asInt64;
+  pulse = args[1].asInt64;
+  strlcpy(response, "success", MyCommandParser::MAX_RESPONSE_SIZE);
+}
+
+bool checkSerialInput(){
+  if (Serial.available()) {
+    char line[128];
+    size_t lineLength = Serial.readBytesUntil('\n', line, 127);
+    line[lineLength] = '\0';
+
+    char response[MyCommandParser::MAX_RESPONSE_SIZE];
+    servoCommandParser.processCommand(line, response);
+    Serial.println(response);
+
+    if (strcmp(response, "success") == 0)
+      return true;
   }
+
+  return false;
+//    if (Serial.available() > 0) {
+//      char symbol = Serial.read();
+//      Serial.print(symbol);
+//    switch (symbol) {
+//      case '0':
+//        pos = 0;
+//        break;
+//      case '3':
+//        pos = 30;
+//        break;
+//      case '9':
+//        pos = 90;
+//        break;
+//      case '5':
+//        pos = 150;
+//        break;
+//      case '8':
+//        pos = 180;
+//        break;
+//      case '+':
+//        pos += 5;
+//        break;
+//      case '-':
+//        pos -= 5;
+//        break;
+//    }
+//  }
+}
+
+void servosSleep(MyCommandParser::Argument *args, char *response) {
+  PWM.sleep();
+}
+
+void servosWake(MyCommandParser::Argument *args, char *response) {
+  PWM.wakeup();
 }
 
 void writePWM(){
+  Serial.printf("Servo %d: Pulse %d\n", servo_id, pulse);
+  PWM.setPWM(servo_id, 0, pulse);
   
-  Serial.print("Pos ");
-  Serial.print(pos);
-  Serial.print(" -> ");
-  Serial.println(pos);
-  PWM.setPWM(0, 0, pos);
-
-//    PWM.setPWM(0, 0, MIN_PULSE + int(0 * ANGLE_FACTOR));
-//    delay(500);  
-//    
-//    PWM.setPWM(0, 0, MIN_PULSE + int(45 * ANGLE_FACTOR));
-//    delay(500);  
 }
